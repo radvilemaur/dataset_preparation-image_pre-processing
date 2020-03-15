43e2c8d3ef4398c6948cc6580f17f4fc7b81214d
radvilemaur <57565417+radvilemaur@users.noreply.github.com>
2020-03-15 12:52:10 +0000

    Update processing.py

--- processing.py
+++ processing.py
@@ -2,8 +2,13 @@ import numpy as np
 import nibabel as nib
 import ants
 
+#n4bias-field correction using ANTSPY 
 img=nib.Nifti1Image.load('0.nii').get_fdata()
 image_1= ants.from_numpy(img, origin=(0,0))
 image_1_n4 = ants.n4_bias_field_correction(image_1)
 image_1_n4_resample =ants.resample_image(image_1_n4, (1,1),False, 4)
 ants.image_write(image_1_n4_resample, '1_processed.nii')
+
+
+#z-score normalization  https://intensity-normalization.readthedocs.io/en/latest/exec.html
+#run directly in bash shell (Ubuntu) 
